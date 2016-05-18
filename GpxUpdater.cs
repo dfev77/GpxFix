6094446c31047987c06208370af7363c1dcdec62
Florin Daneliuc <florin.daneliuc@gmail.com>
2016-05-18 20:48:59 +0300

    initial version

---
+++ GpxUpdater.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+
+namespace GpxFix
+{
+    class GpxUpdater
+    {
+        private const string DefaultNamespace = "http://www.topografix.com/GPX/1/1";
+
+        public void Process(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File '{0}' does not exist!", filePath);
+                return;
+            }
+
+            Console.WriteLine("Updating '{0}'", filePath);
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filePath);
+
+            AddDate(doc);
+            string newPath = GetOutputFileName(filePath);
+
+            doc.Save(newPath);
+
+            Console.WriteLine("New file written to '{0}'", newPath);
+        }
+
+        private void AddDate(XmlDocument gpxDocument)
+        {
+            DateTime date = new DateTime(1900, 1, 1);
+            XmlNamespaceManager nsmanager = new XmlNamespaceManager(gpxDocument.NameTable);
+            nsmanager.AddNamespace("x", DefaultNamespace);
+
+            XmlNodeList nodesList = gpxDocument.SelectNodes("/x:gpx/x:trk/x:trkseg/x:trkpt", nsmanager);
+            foreach (XmlNode node in nodesList)
+            {
+                XmlElement newNode = gpxDocument.CreateElement("time", gpxDocument.DocumentElement.NamespaceURI);
+                newNode.InnerText = date.ToString("s") + "Z";
+
+                node.AppendChild(newNode);
+                date = date.AddSeconds(1);
+            }
+        }
+
+        private string GetOutputFileName(string inputFileName)
+        {
+            FileInfo fileInfo = new FileInfo(inputFileName);
+            string newPath = Path.Combine(fileInfo.Directory.FullName,
+                                                    Path.GetFileNameWithoutExtension(fileInfo.Name) + ".updated");
+            newPath = Path.ChangeExtension(newPath, fileInfo.Extension);
+
+            return newPath;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace GpxFix
+{
+    public static class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                if (args.Length == 1)
+                {
+                    GpxUpdater gpxUpdater = new GpxUpdater();
+                    gpxUpdater.Process(args[0]);
+                    return 0;
+                }
+                else
+                {
+                    Console.WriteLine("Missing input file");
+                    Console.WriteLine("Usage: GpxFix <gpx file path>");
+                    return -1;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error processing file: {0}", ex);
+                return -2;
+            }
+        }
+
+    }
+}
